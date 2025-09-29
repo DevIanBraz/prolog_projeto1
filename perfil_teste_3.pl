86af77f78083fd58a9c0f948d62218cbee664e1b
Ian Carlo Araújo Braz <devianbraz@gmail.com>
2025-09-28 23:29:52 -0300

    Update and rename perfil_teste_3 to perfil_teste_3.pl

---
+++ perfil_teste_3.pl
@@ -0,0 +1,21 @@
+% Perfil: Aluno com pensamento crítico e interesse em segurança (Segurança da Informação)
+resposta(1, s).  % Lógica de programação
+resposta(2, n).  % Matemática e estatística
+resposta(3, s).  % Algoritmos complexos
+resposta(4, s).  % Programação
+resposta(5, n).  % Design UI/UX
+resposta(6, n).  % Banco de dados
+resposta(7, n).  % Criatividade
+resposta(8, n).  % Trabalho em equipe
+resposta(9, s).  % Redes de computadores
+resposta(10, s). % Pensamento crítico
+resposta(11, s). % Atenção a detalhes
+resposta(12, s). % Ética hacking
+resposta(13, s). % Análise de riscos
+resposta(14, n). % Curiosidade científica
+resposta(15, n). % Análise de dados
+resposta(16, n). % Visualização de dados
+resposta(17, s). % Sistemas operacionais
+resposta(18, s). % Infraestrutura cloud
+resposta(19, s). % Organização
+resposta(20, n). % Pensamento abstrato
