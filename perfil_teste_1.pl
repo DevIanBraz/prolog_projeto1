e7733885b2d80e47a624168dc9c9e4550122d4be
Ian Carlo Araújo Braz <devianbraz@gmail.com>
2025-09-28 23:17:18 -0300

    Create perfil_teste_1.pl

---
+++ perfil_teste_1.pl
@@ -0,0 +1,15 @@
+% Perfil: Aluno com forte inclinação para dados e matemática
+resposta(matematica_estatistica, s).
+resposta(analise_dados, s).
+resposta(programacao, s).
+resposta(visualizacao_dados, s).
+resposta(curiosidade_cientifica, s).
+resposta(logica_programacao, s).
+resposta(algoritmos_complexos, s).
+resposta(pensamento_abstrato, s).
+resposta(design_uiux, n).
+resposta(trabalho_equipe, n).
+resposta(redes_computadores, n).
+resposta(etica_hacking, n).
+resposta(infraestrutura_cloud, n).
+resposta(organizacao, s).
