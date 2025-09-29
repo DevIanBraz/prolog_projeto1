b9e993bcdc6fbc004bf5210b01e9e4587136234f
Ian Carlo Araújo Braz <devianbraz@gmail.com>
2025-09-28 23:29:06 -0300

    Update perfil_teste_1.pl

--- perfil_teste_1.pl
+++ perfil_teste_1.pl
@@ -1,15 +1,21 @@
-% Perfil: Aluno com forte inclinação para dados e matemática
-resposta(matematica_estatistica, s).
-resposta(analise_dados, s).
-resposta(programacao, s).
-resposta(visualizacao_dados, s).
-resposta(curiosidade_cientifica, s).
-resposta(logica_programacao, s).
-resposta(algoritmos_complexos, s).
-resposta(pensamento_abstrato, s).
-resposta(design_uiux, n).
-resposta(trabalho_equipe, n).
-resposta(redes_computadores, n).
-resposta(etica_hacking, n).
-resposta(infraestrutura_cloud, n).
-resposta(organizacao, s).
+% Perfil: Aluno com forte inclinação para dados e matemática (Ciência de Dados)
+resposta(1, s).  % Lógica de programação
+resposta(2, s).  % Matemática e estatística
+resposta(3, s).  % Algoritmos complexos
+resposta(4, s).  % Programação
+resposta(5, n).  % Design UI/UX
+resposta(6, s).  % Banco de dados
+resposta(7, n).  % Criatividade
+resposta(8, n).  % Trabalho em equipe
+resposta(9, n).  % Redes de computadores
+resposta(10, s). % Pensamento crítico
+resposta(11, s). % Atenção a detalhes
+resposta(12, n). % Ética hacking
+resposta(13, n). % Análise de riscos
+resposta(14, s). % Curiosidade científica
+resposta(15, s). % Análise de dados
+resposta(16, s). % Visualização de dados
+resposta(17, n). % Sistemas operacionais
+resposta(18, n). % Infraestrutura cloud
+resposta(19, s). % Organização
+resposta(20, s). % Pensamento abstrato
